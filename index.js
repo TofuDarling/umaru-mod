bc4957dba2dd5d224a72df61b62f12fac3a92558
aqxa-2045 <89077038+aqxa-2045@users.noreply.github.com>
2021-08-18 02:21:52 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const config = require('./config.json')
+const command = require('./command')
 
 console.clear()
 
 client.on('ready', ()=> {
     console.log('Umaru Is Online!')
-})
+
+    command(client, 'ping', message =>{
+        message.channel.send("Pong!")
+    }) 
+    command(client, 'version', message => {
+        message.channel.send("You are currently running version 0.2!")
+    })
+})  
 
 client.login(config.token)
