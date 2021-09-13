7b5272ca3acb4b749b4541b2cdeef95deddb86e4
aqxa-2045 <89077038+aqxa-2045@users.noreply.github.com>
2021-09-13 20:28:45 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const config = require('./config.json')
 const command = require('./command')
+const mongo = require('./mongo')
 
 console.clear()
 
-client.on('ready', ()=> {
+client.on('ready', async ()=> {
     console.log('Umaru Is Online!')
+    
+    await mongo().then(mongoose => {
+      try {
+        console.log('Connected To MongoDB!')
+      } finally {
+        mongoose.connection.close()
+      }
+    })
+  
 
     command(client, 'ping', message =>{
         message.channel.send("Pong!")
@@ -15,7 +25,7 @@ client.on('ready', ()=> {
         message.channel.send("You are currently running version 0.2!")
     })
     command(client, 'help', (message) =>{
-        message.channel.send("My current commands are: ?ping, ?version, ?servers, ?cc/clearchannel), ?ban, ?kick")
+        message.channel.send("My current commands are: ?ping, ?version, ?servers, ?cc/clearchannel), ?ban, ?kick, ?createtextchannel, ?createvoicechannel.")
     })
     command(client, 'servers', (message) => {
         client.guilds.cache.forEach((guild) => {
@@ -31,17 +41,6 @@ client.on('ready', ()=> {
             message.channel.bulkDelete(results)
          })
         }
-      })
-    
-      command(client, 'status', (message) => {
-        const content = message.content.replace('?status ', '')
-    
-        client.user.setPresence({
-          activity: {
-            name: content,
-            type: 0,
-          },
-        })
       })
     })
     command(client, 'ban', (message) => {
@@ -89,8 +88,44 @@ client.on('ready', ()=> {
         message.channel.send(
           `${tag} You do not have permission to use this command!`
         )
-      }
+      } 
     })
-
-
+    command(client, 'createtextchannel', (message) => {
+      const name = message.content.replace('?createtextchannel ', '')
+  
+      message.guild.channels
+        .create(name, {
+          type: 'text',
+        })
+        .then((channel) => {
+          const categoryId = '719799253706342421'
+          channel.setParent(categoryId)
+        })
+    })
+    command(client, 'createvoicechannel', (message) => {
+      const name = message.content.replace('!createvoicechannel ', '')
+  
+      message.guild.channels
+        .create(name, {
+          type: 'voice',
+        })
+        .then((channel) => {
+          const categoryId = '719799253706342421'
+          channel.setParent(categoryId)
+          channel.setUserLimit(10)
+        })
+    })
+    command(client, 'whoiscurrynoodles', (message) => {
+      message.channel.send("A very cracked player :3. yt: currynoodles")
+    })
+    command(client, 'whoisaqxa', (message) => {
+      message.channel.send("Hes my dad. He is very cracked at bedwars... and if u dont think so... o-o")
+    })
+    command(client, 'urmum', (message) => {
+      message.channel.send("Your mums house ;)")
+    })
+    command(client, 'whoistom', (message) => {
+      message.channel.send("Hes the most epik gamer. he makes tik tok vids! check him out at @beasn!")
+    })
+    
 client.login(config.token)
